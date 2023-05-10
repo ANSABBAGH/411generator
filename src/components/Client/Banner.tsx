b94ef0ffd9aa944373303ef090f0fb6eafa57732
Anthony Sabbagh <anthonysabbagh03@gmail.com>
2023-05-11 00:51:37 +0200

    fixup!

--- src/components/Client/Banner.tsx
+++ src/components/Client/Banner.tsx
@@ -1,14 +1,18 @@
+import React from "react";
+
 interface BannerProps {
-  imgUrl: string
+  imgUrl: string;
 }
 
 export const Banner = ({ imgUrl }: BannerProps) => {
   return (
-    <div className="autofitimg" id="autofitting2">
-      <img
-        src={imgUrl}
-        alt="Massothérapie Clinique Les 4 Mains"
-      />
-    </div>
+    <>
+      <div className="autofitimg" id="autofitting2">
+        <img src={imgUrl} alt="Massothérapie Clinique Les 4 Mains" />
+      </div>
+      <div className="rpcq font2_reg">
+        <h1>RÉSEAU DES PROFESSIONNELS DE LA SANTÉ DU QUÉBEC (RPSQ) - Thérapeutes et autres</h1>
+      </div>
+    </>
   );
 };
